01bc01071b43e1dc1ae9afce9f2f8493b8ec585c
TayyibS1998 <tayyib.saddique@digital.trade.gov.uk>
2023-11-29 16:48:24 +0000

    adding docustrings

--- db_utils.py
+++ db_utils.py
@@ -38,27 +38,35 @@ class DataFrameInfo():
         self.df = df
 
     def info(self):
+        '''Returns general info about the dataframe'''
         return self.df.info()
     
     def describe(self):
+        '''Returns descriptive statistics for the dataframe'''
         return self.df.describe()
     
     def shape(self):
+        '''Returns shape of the dataframe'''
         return self.df.shape()
     
     def data_types(self):
+        '''Returns all datatypes'''
         return self.df.dtypes
     
     def column_describe(self, col : pd.Series):
+        '''Highlights basic descriptive statistics for a column'''
         return self.df[col].describe()
 
     def count_nulls_in_columns(self, col :pd.Series):
+        '''Returns the total count of NaN values in a column'''
         return self.df[col].isnull().sum()
     
     def percent_nulls_in_columns(self, col: pd.Series):
+        '''Returns the percentage of NaN values in a column'''
         return (self.df[col].isnull().sum() / self.df[col].size)
 
     def outliers_in_columns(self, col: pd.Series):
+        '''Highlights outliers in column based on bounds from quartiles'''
         Q1 = self.df[col].quantile(0.25)
         Q3 = self.df[col].quantile(0.75)
         IQR = Q3 - Q1
@@ -72,36 +80,41 @@ class DataTransform():
         self.df = df
     
     def convert_to_float(self, col):
+        '''Converts all values in column into float'''
         self.df[col] = self.df[col].astype(float)
         
     def convert_to_datetime(self, col):
+        '''Forcefully converts all values in column into datetime format'''
         self.df[col] = pd.to_datetime(self.df[col], errors='coerce')
-
-
 class DataFrameTransform():
     def __init__(self, df : pd.DataFrame):
         self.df = df
 
     def impute_mean(self, col : pd.Series):
+        '''Replaces all NaN values with the mean value of the column'''
         mean = self.df[col].mean()
         self.df[col].fillna(mean, inplace= True)
         return self.df[col]
 
     def impute_median(self, col : pd.Series):
+        '''Replaces all NaN values with the median value of the column'''
         median = self.df[col].median()
         self.df[col].fillna(median, inplace= True)
         return self.df[col]
     
     def log_transform(self, col: pd.Series):
+        '''Transforms distribution of data using log transformation'''
         new_col = self.df[col].map(lambda i: np.log(i) if i > 0.0 else 0.0)
         return self.df[new_col]
     
     def remove_outliers(self, col: pd.Series):
+        '''Removes any outliers from dataframe'''
         outliers = DataFrameInfo.outliers_in_columns(self, col)
         self.df = self.df.drop(outliers)
         return self.df[col]
     
     def remove_correlated_columns(self, threshold = 0.95):
+        '''Removes any columns with a correlation threshold > 0.95 by default unless otherwise specified'''
         col_corr = set()
         corr_matrix = Plotter.visualise_correlation_matrix(self).abs()
         for i in range(len(corr_matrix.columns)):
@@ -118,21 +131,25 @@ class Plotter():
         self.df = df
 
     def qq_plot(self, col: pd.Series):
-        """Generates Q-Q plot for column data."""
+        '''Generates QQ Plot for column'''
         return sm.qqplot(self.df[col])
      
     def histogram(self, col : pd.Series):
+        '''Generates Histogram for column'''
         return sns.histplot(self.df[col])
         
-    def visualise_removed_NaN(self):
+    def visualise_NaN(self):
+        '''Visualises NaN data'''
         return msno.bar(self.df)
 
     def visualise_skew(self, col: pd.Series):
+        '''Visualises skew in distribution'''
         skew = self.df[col].skew()
         ax = self.df[col].plot.kde(bw_method=0.5)
         return skew, ax
 
     def visualise_outliers(self, col: pd.Series):
+        '''Visualises outliers using boxplots showing transformation in data'''
         fig, axs = plt.subplots(nrows=1, ncols=2)
         flierprops = dict(marker='o', markerfacecolor='green', markersize=2,
                   linestyle='none')
@@ -146,6 +163,7 @@ class Plotter():
         return fig, axs
     
     def visualise_correlation_matrix(self):
+        '''Visualises correlation matrix'''
         return self.df.corr()
 
 
